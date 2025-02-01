ce7ca1d0c410cb4ded255316617d514fac71d17f
BlastChaos <76956740+BlastChaos@users.noreply.github.com>
2025-02-01 18:21:00 -0500

    wip

--- LifeMon/Models/Models.cs
+++ LifeMon/Models/Models.cs
@@ -23,19 +23,28 @@ public class LifeMon
 
     [BsonElement("name")]
     public required string Name { get; set; }
+
+    [BsonElement("speed")]
+    public required int Speed { get; set; }
+
+    [BsonElement("hp")]
+    public required int Hp { get; set; }
+
+    [BsonElement("attack")]
+    public required int Attack { get; set; }
 }
 
 public class Team
 {
     [BsonId]
     public ObjectId Id { get; set; }
-    
+
     [BsonElement("userId")]
     public required ObjectId UserId { get; set; }
-    
+
     [BsonElement("name")]
     public required string Name { get; set; }
-    
+
     [BsonElement("lifemons")]
     public required List<ObjectId> LifeMons { get; set; }
 }
@@ -70,13 +79,34 @@ public class LifeMonBattle
     public required bool IsDead { get; set; }
     public required int? AttackBoost { get; set; }
     public required int? DefenseBoost { get; set; }
+    public required bool IsInTheGame { get; set; }
 
 }
 
 public class BattleInfo
 {
+    public required string Player1Id { get; set; }
+    public required string Player2Id { get; set; }
     public required LifeMonBattle[] Player1LifeMons { get; set; }
     public required LifeMonBattle[] Player2LifeMons { get; set; }
+    public required Turn[] Turns { get; set; }
+}
+
+public class Turn
+{
+    public TurnInfo? Player1Turn { get; set; }
+    public TurnInfo? Player2Turn { get; set; }
+}
+
+
+
+public class TurnInfo
+{
+    public required string PlayerId { get; set; }
+    public required TurnType TurnType { get; set; }
+    public required string PokemonId { get; set; }
+    public required string? AttackId { get; set; }
+    public required string? NewPokemonId { get; set; }
 }
 
 public enum TurnType
